b20a0c03eee8d66e92c0b4995b7e9fe34794cae8
paw145@humboldt.edu and Tanvi Garg <paw145@humboldt.edu@>
2019-01-29 09:44:24 -0800

    add list of categories and display in html

--- src/app/category-list/category-list.component.ts
+++ src/app/category-list/category-list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Category } from '../models/category.model.ts'
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent implements OnInit {
+export class CategoryListComponent {
+  categories: Category[] = [
+    new Category("Housing", 1),
+    new Category("Jobs", 2),
+    new Category("Furniture", 3)
+  ];
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
 }
